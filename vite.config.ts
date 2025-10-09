5a5917b915fd3d3efedc423929846d95146a0ab2
Jasson López <jasson.lopez@elaniin.com>
2025-10-08 22:09:03 -0600

    tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
